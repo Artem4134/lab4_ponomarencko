5de903ed24cb94cfab7ae72eca0dcb08716cc760
Artem4134 <artem.ponomarencko2018@yandex.ru>
2026-02-02 17:09:05 +0300

    Ленивый коммит

--- server/Program.cs
+++ server/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Лабораторная работа 4");
+        Console.WriteLine("ФИО: Пономаренко Артём Юрьевич");
+        Console.WriteLine("Группа: ИСП-231");
+        Console.WriteLine(DateTime.Now);
+        
+        bool exit = false;
+        
+        while (!exit)
+        {
+            Console.WriteLine("\nМЕНЮ");
+            Console.WriteLine("1 - ФИО");
+            Console.WriteLine("2 - ГРУППА");
+            Console.WriteLine("3 - ДАТА");
+            Console.WriteLine("4 - ВЫХОД");
+            Console.Write("\nВыберите опцию: ");
+            
+            string choice = Console.ReadLine();
+            
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine($"\nФИО: Пономаренко Артём Юрьевич");
+                    break;
+                case "2":
+                    Console.WriteLine($"\nГруппа: ИСП-231");
+                    break;
+                case "3":
+                    Console.WriteLine($"\nТекущая дата и время: {DateTime.Now}");
+                    break;
+                case "4":
+                    Console.WriteLine("\nВыход из программы...");
+                    exit = true;
+                    break;
+                default:
+                    Console.WriteLine("\nНеверный выбор. Попробуйте снова.");
+                    break;
+            }
+        }
+    }
+}
